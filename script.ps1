7460b78d9adeb64a34222f8b9daaf2ea80c8b660
Jerry Zheng <zql0301@gmail.com>
2025-08-29 01:16:02 +0800

    update

--- script.ps1
+++ script.ps1
@@ -1,42 +1,70 @@
-$projectPath = "$home\desktop\tysgs"
-cd $projectPath
+$patchPath = "$home\desktop\修订7"
+cd $patchPath
 
-$sidebarmd = @()
-$sidebarmd += "* [首页](README.md)"
+$projectPath = "D:\Repository\MyProject\tysgs"
 
-$assetsPath = "E:\115\tysgs_assets"
-echo "tysgs assets path: $assetsPath"
-#ls $assetsPath -Recurse | ? {!$_.PSIsContainer} | Copy-Item -Destination .\docs\assets\ -Force -Confirm:$false
+$assetsPath = Join-Path $projectPath "docs\assets"
+$pagesPath = Join-Path $projectPath "docs\pages"
 
-$folderList = (ls $assetsPath | ? { $_.PSIsContainer }).Name    #晋
-foreach ($folder in $folderList) {
-    $folderPath = Join-Path -Path $assetsPath $folder
+foreach ($file in Get-ChildItem -Path $patchPath) {
+    if ($file.Name -match "^技标") {
+        $jiBiao = $true
+        $newName = $file.Name.replace("技标-","").replace("技标-","").replace("】【","-").replace("】","-").replace("【","-").trimend("-")
+        $file | Rename-Item -NewName $newName
+        echo "Renamed $($file.Name) to $newName"
 
-    $baseList = (ls $folderPath).BaseName
-    $fileList = (ls $folderPath).Name
-    $generalCount = (ls $folderPath).Count
-
-    $sidebarmd += "---`r`n"
-    $sidebarmd += "  * $folder ($generalCount)"
-
-    for ($i = 0; $i -lt $baseList.Count; $i++) {
-        $fileName = $fileList[$i]   #晋-杜预001.jfif
-        $baseName = $baseList[$i]     #晋-杜预001
-        $mdName = $baseName + ".md"
-        $sidebarmd += "    * [$baseName](pages/$mdName)"
-
-        $baseMd = @()
-        $baseMd += "# $baseName`r`n"
-        $baseMd += "![$baseName](../assets/$fileName)`r`n"
-        $baseMd | Out-File -FilePath ".\docs\pages\$mdName" -Encoding utf8 -Force -Confirm:$false
+        $fileBaseName = $newName.split(".")[0]
+        $fileExt = ".jpg"
+        $fileName = $newName
+    } else {
+        $jiBiao = $false
+        $fileBaseName = $file.BaseName
+        $fileExt = ".jpg"
+        $fileName = $file.Name
     }
 
-   # $sidebarmd += "`r"
+    # 如果jfif在assets里已存在
+    if ((Test-Path $(Join-Path $assetsPath $fileName)) -eq $true) {
+        echo "$fileName already exists in $assetsPath, overwriting..."
+        Copy-Item -Path $fileName -Destination $assetsPath -Force
+    }
+
+    # 如果jfif在assets里不存在但jpg存在
+    if (((Test-Path $(Join-Path $assetsPath $fileName)) -eq $false) -and ((Test-Path $(Join-Path $assetsPath $fileBaseName$fileExt)) -eq $true)) {
+        echo "$fileBaseName$fileExt exists, remove and copy jfif"
+        Copy-Item -Path $fileName -Destination $assetsPath -Force
+        Remove-Item -Path $(Join-Path $assetsPath $fileBaseName$fileExt) -Force
+
+        #修改md文件
+        Get-Content $("$pagesPath\$fileBaseName" + ".md") | ForEach-Object { $_ -replace ".jpg",".jfif" } | Set-Content $("$pagesPath\$fileBaseName" + ".md")
+        echo "Updated $fileBaseName md to reference jfif"
+    }
+
+    # 如果jfif在assets里不存在jpg也不存在
+    if (((Test-Path $(Join-Path $assetsPath $fileName)) -eq $false) -and ((Test-Path $(Join-Path $assetsPath $fileBaseName$fileExt)) -eq $false)) {
+        echo "$fileName does not exist, copying..."
+        Copy-Item -Path $fileName -Destination $assetsPath -Force
+
+        if ($jiBiao -eq $true) {
+            echo "手动加技标到md里"
+            continue
+        }
+
+        #增加md文件
+        New-Item -Path $("$pagesPath\$fileBaseName" + ".md") -ItemType File -Force
+
+        $mdContent = @"
+# $fileBaseName
+
+![$fileBaseName](../assets/$($fileBaseName + ".jfif"))
+    
+"@
+        $mdContent | Set-Content -Path $("$pagesPath\$fileBaseName" + ".md")
+        echo "Created new md file for $fileBaseName"
+        echo "手动加到_sidebar里"
+    }
 }
 
-$sidebarmd += "---"
 
-$sidebarmd | Out-File -FilePath ".\docs\_sidebar.md" -Encoding utf8 -Force -Confirm:$false
-
-# $comment = "update "; cd $projectPath; git add .; git commit -m $comment; git push origin main
+# cd "D:\Repository\MyProject\tysgs"; $comment = "update "; cd $projectPath; git add .; git commit -m $comment; git push origin main
 
